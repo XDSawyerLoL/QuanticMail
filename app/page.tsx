20ea5cc2f0985939be46d6769743038b4e1e22ee
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-16 17:23:51 +0200

    chore(v0.9): keep primary floating action uncluttered

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,9 @@ export default function Home() {
   return (
     <>
       <QuanticNetworkApp />
-      <div className="qn-fab-stack">
-        <Link className="qn-vault-fab" href="/devices" title="Gérer les appareils Quantic">
-          Appareils
-        </Link>
-        <Link className="qn-vault-fab" href="/vault" title="Sauvegarder ou restaurer mon identité Quantic">
-          Identity Vault
-        </Link>
-      </div>
+      <Link className="qn-vault-fab" href="/vault" title="Sauvegarder ou restaurer mon identité Quantic">
+        Identity Vault
+      </Link>
     </>
   );
 }
