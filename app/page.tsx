609cd0230610b0633fa7d12850dd1ef019622d42
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-16 20:45:13 +0200

    fix: combine V1 app with relay settings entry

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import { QuanticNetworkApp } from "@/components/quantic-network-app";
+import { QuanticNetworkV1App } from "@/components/quantic-network-v1-app";
 
 export default function Home() {
   return (
     <>
-      <QuanticNetworkApp />
+      <QuanticNetworkV1App />
       <Link
         className="qn-vault-fab"
         href="/network"
