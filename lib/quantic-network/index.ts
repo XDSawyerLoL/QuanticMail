9d267ff0ef3d1d3ee64a6758bcd223d7d13fe01a
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-17 22:46:02 +0200

    feat: add Quantic Network public facade

---
+++ lib/quantic-network/index.ts
@@ -0,0 +1,4 @@
+export { isCanonicalQuanticAddress, normalizeQuanticLocator } from "./addressing.mjs";
+export type { QuanticAppCapability, QuanticAppCapabilityPayload } from "../quantic-core/app-capability";
+export { createAppCapability, verifyAppCapability } from "../quantic-core/app-capability";
+export type { RelayEndpoint } from "../quantic/relay-client";
