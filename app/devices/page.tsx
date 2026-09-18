51b61d67e196455304d0b083307249e658274dd9
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-18 22:48:19 +0200

    fix(mail): rely on layout for global nav

--- app/devices/page.tsx
+++ app/devices/page.tsx
@@ -1,6 +1,5 @@
-import { QuanticGlobalNav } from "@/components/quantic-global-nav";
 import { DevicesV11App } from "@/components/devices-v11-app";
 
 export default function DevicesPage() {
-  return <><QuanticGlobalNav /><DevicesV11App /></>;
+  return <DevicesV11App />;
 }
