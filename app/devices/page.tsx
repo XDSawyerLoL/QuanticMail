f91b563176046aff33acaad201922f2ca243eecf
XDSawyerLoL <74513223+XDSawyerLoL@users.noreply.github.com>
2026-09-18 12:38:08 +0200

    feat: share Quantic navigation on devices/page.tsx

--- app/devices/page.tsx
+++ app/devices/page.tsx
@@ -1,5 +1,6 @@
+import { QuanticGlobalNav } from "@/components/quantic-global-nav";
 import { DevicesV11App } from "@/components/devices-v11-app";
 
 export default function DevicesPage() {
-  return <DevicesV11App />;
+  return <><QuanticGlobalNav /><DevicesV11App /></>;
 }
